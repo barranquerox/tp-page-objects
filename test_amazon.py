cbfab45c44c3faff8a15a8bf43ca579836eca2a9
Luis Garelli <luiscarlosgarelli@gmail.com>
2022-04-28 14:39:30 +0200

    first page object

--- test_amazon.py
+++ test_amazon.py
@@ -1,7 +1,19 @@
+from page_object.HomePage import HomePage
 from selenium import webdriver
+
 
 def test_amazon1():
     driver = webdriver.Chrome()
     driver.get("https://www.amazon.fr/")
     driver.maximize_window()
     driver.quit()
+
+
+def test_page_object():
+    driver = webdriver.Chrome()
+    driver.get("https://www.amazon.fr/")
+    home_page = HomePage(driver)
+    home_page.closeCookies()
+    home_page.openAllMenu()
+    home_page.openBookCategory()
+    home_page.openAllBooks()
