5941f370e7e35c88d1c7573ddd9b7d5b2dff0aaa
Luis Garelli <luiscarlosgarelli@gmail.com>
2022-04-29 09:44:42 +0200

    added comment

--- test_amazon.py
+++ test_amazon.py
@@ -12,8 +12,13 @@ def test_amazon1():
 def test_page_object():
     driver = webdriver.Chrome()
     driver.get("https://www.amazon.fr/")
+
     home_page = HomePage(driver)
     home_page.closeCookies()
     home_page.openAllMenu()
     home_page.openBookCategory()
     home_page.openAllBooks()
+
+    ## example draft PR
+
+
